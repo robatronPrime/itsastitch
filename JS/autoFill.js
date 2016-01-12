6a9f10d94b7adf9a84b2baa8200cb1f2385382bb
Robert <robstow94@gmail.com>
2016-01-12 02:00:28 +0000

    update-12/1/16

---
+++ JS/autoFill.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MyPlanner.co.uk: Home Page</title>
+    <link rel="stylesheet" type="text/css"
+        href="unsemantic-grid-responsive-tablet.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="stylesheet" 
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+    
+<body>
+    
+    <section id="Logo" class="grid-100 tablet-grid-100 mobile-grid-100">
+        <img src="images/logo1.png" alt="logo">
+    </section>
+    
+    <section id="content" class="grid-25 tablet-grid-100 mobile-grid-100">
+        
+        <section id="ti
---
+++ SQL/tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ConForm (
+    id    int primary key auto_increment,
+    firstname varchar(20) not null,
+    lastname varchar(20) not null,
+    email varchar(40) not null,
+    message varchar(400)
+);
+
